a93e771d0a6a49fa4887a651290c33501932b53a
Maksim Lebedev <maksim.lebedev@Maksims-MacBook-Pro.local>
2022-11-12 13:37:24 +0100

    Solving bouncing

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,16 @@
 # bounce.py
 #
 # Exercise 1.5
+
+INIT_HEIGHT = 100
+FADING = 3 / 5
+
+number_of_bounces = int(input('Input number of bounces: '))
+h = None
+
+for i in range(1, number_of_bounces + 1):
+     if not h:
+         h = INIT_HEIGHT
+    
+     h *= FADING
+     print(i, h)
